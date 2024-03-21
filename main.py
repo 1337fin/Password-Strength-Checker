7eef0f02421cf4069b2105c95387f44375e2f24b
fin702106 <119663332+fin702106@users.noreply.github.com>
2024-03-21 13:06:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from PasswordChecker import PasswordChecker
 
+# Author: fin702106
+# Date: 21/03/2024
+# File: ~/PasswordChecker/main.py
+
 password: str = input("Password > ")
 
 checker = PasswordChecker(password)
